5286d9ffc1aaaa12490cd0aea3f9a7c626a526d4
Hector Lemus <dev.hectorlemus@gmail.com>
2020-06-22 11:10:49 -0500

    'flutter_secure_storage' is implemented

--- lib/src/repository/user_repository.dart
+++ lib/src/repository/user_repository.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserRepository {
+  final storage = new FlutterSecureStorage();
+
   Future<String> authenticate({
     @required String username,
     @required String password,
@@ -11,20 +14,23 @@ class UserRepository {
 
   Future<void> deleteToken() async {
     /// delete from keystore/keychain
-    await Future.delayed(Duration(seconds: 1));
+    /// await Future.delayed(Duration(seconds: 1));
+    await storage.delete(key: 'token');
     return;
   }
 
   Future<void> persistToken(String token) async {
     /// write to keystore/keychain
     /// Implement flutter_secure_storage
-    await Future.delayed(Duration(seconds: 1));
+    /// await Future.delayed(Duration(seconds: 1));
+    await storage.write(key: 'token', value: token);
     return;
   }
 
   Future<bool> hasToken() async {
     /// read from keystore/keychain
-    await Future.delayed(Duration(seconds: 1));
-    return false;
+    /// await Future.delayed(Duration(seconds: 1));
+    final token = await storage.read(key: 'token');
+    return token != null;
   }
 }
