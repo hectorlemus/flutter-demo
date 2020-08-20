e351c703e92d9fc602329db8c897dbfd7a0cd0ed
Hector Lemus <dev.hectorlemus@gmail.com>
2020-08-20 12:59:35 -0500

    Refactoring

--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -20,48 +20,70 @@ class _CounterPageState extends State<CounterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Counter page'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              StreamBuilder<Object>(
-                  stream: _bloc.counterStream,
-                  initialData: 0,
-                  builder: (context, snapshot) {
-                    return Text(
-                      '${snapshot.data}',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  }),
-            ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Counter page'),
+      ),
+      body: _body(),
+      floatingActionButton: _actions(),
+    );
+  }
+
+  Widget _body() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
           ),
+          _counter(),
+        ],
+      ),
+    );
+  }
+
+  Widget _counter() {
+    return StreamBuilder<Object>(
+      stream: _bloc.counterStream,
+      initialData: 0,
+      builder: (context, snapshot) {
+        return Text(
+          '${snapshot.data}',
+          style: Theme.of(context).textTheme.headline4,
+        );
+      },
+    );
+  }
+
+  Widget _actions() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        _clearCounter(),
+        SizedBox(
+          width: 10,
         ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              heroTag: 'clearCounter',
-              onPressed: () => _bloc.sendEvent.add(ClearCounter()),
-              tooltip: 'clear',
-              child: Icon(Icons.clear),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              heroTag: 'incrementClearCounter',
-              onPressed: () => _bloc.sendEvent.add(IncremenrCounter()),
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            ),
-          ],
-        ));
+        _incrementClearCounter(),
+      ],
+    );
+  }
+
+  Widget _clearCounter() {
+    return FloatingActionButton(
+      heroTag: 'clearCounter',
+      onPressed: () => _bloc.sendEvent.add(ClearCounter()),
+      tooltip: 'clear',
+      child: Icon(Icons.clear),
+    );
+  }
+
+  Widget _incrementClearCounter() {
+    return FloatingActionButton(
+      heroTag: 'incrementClearCounter',
+      onPressed: () => _bloc.sendEvent.add(IncremenrCounter()),
+      tooltip: 'Increment',
+      child: Icon(Icons.add),
+    );
   }
 }
